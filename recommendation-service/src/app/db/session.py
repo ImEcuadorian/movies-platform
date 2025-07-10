b10549e797ea5056062b0ec9648e82ce8712c64d
Mauricio Saldarriaga Morales <hugosmm2000@gmail.com>
2025-07-10 09:16:13 -0500

    Update database connection settings and enhance Dockerfile configuration

--- recommendation-service/src/app/db/session.py
+++ recommendation-service/src/app/db/session.py
@@ -1,6 +1,17 @@
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from src.app.core.config import settings
 
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URL, pool_size=10, max_overflow=20, future=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    settings.SQLALCHEMY_DATABASE_URL,
+    pool_size=100,
+    max_overflow=500,
+    future=True,
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
--- recommendation-service/src/app/main.py
+++ recommendation-service/src/app/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
-
 from src.app.core.config import settings
-from src.app.db.base import Base
 from src.app.db.session import engine
+from src.app.db.base import Base
 from src.app.api.v1.recommendation import router as rec_router
 
+# Crea esquemas al arrancar
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title=settings.PROJECT_NAME)
+
+@app.get("/health", include_in_schema=False)
+async def health_check():
+    return {"status": "ok"}
 
 app.include_router(rec_router, prefix="/api/v1")
